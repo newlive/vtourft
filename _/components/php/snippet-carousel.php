bccb3a643f7036591717043c1fe7a8f4613ee894
rizki <rizki_newlive@yahoo.com>
2014-06-07 12:25:17 +0700

    home jadi

---
+++ _/components/php/snippet-carousel.php
@@ -0,0 +1,58 @@
+<div id="myCarousel" class="carousel hidden-sm slide">
+
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+		<li data-target="#myCarousel" data-slide-to="3"></li>
+		<li data-target="#myCarousel" data-slide-to="4"></li>
+		<li data-target="#myCarousel" data-slide-to="5"></li>
+	</ol><!-- carousel-indicators -->
+
+	<section class="carousel-inner container-fluid">
+		
+      	<div class="active item"><img src="images/carousel/seattle_skyline.jpg" alt="Photo of Seattle Skyline">
+			<div class="carousel-caption">
+				<h3>Pendidikan Teknik Elektronika</h3>				
+			</div>
+		</div>
+		<div class="item"><img src="images/carousel/jewelry_workshop.jpg" alt="Photo of jewelry_workshop" class="item1">
+			<div class="carousel-caption">
+				<h3>Pendidikan Teknik Elektro</h3>
+			</div>
+		</div>
+		<div class="item"><img src="images/carousel/painting_workshop.jpg" alt="Photo of painting_workshop">
+			<div class="carousel-caption">
+					<h3>Pendidikan Teknik Sipil dan Perencanaan</h3>
+			</div>
+		</div>
+		<div class="item"><img src="images/carousel/pottery_workshop.jpg" alt="Photo of pottery_workshop">
+			<div class="carousel-caption">
+					<h3>Pendidikan Teknik Boga dan Busana</h3>
+			</div>
+		</div>
+		<div class="item"><img src="images/carousel/quicksketch_workshop.jpg" alt="Photo of quicksketch_workshop">
+			<div class="carousel-caption">
+					<h3>Pendidikan Teknik Mesin</h3>
+			</div>
+		</div>
+		<div class="item"><img src="images/carousel/letterpress_studio.jpg" alt="Photo of letterpress_studio">
+			<div class="carousel-caption">
+					<h3>Pendidikan Teknik Otomotif</h3>
+			</div>
+		</div>
+		
+	</section><!-- carousel-inner -->
+	
+	<form class="col-sm-4" id="searchForm">
+	    <div class="form-group col-sm-8 col-sm-offset-4">
+	      <div class="input-group input-group-lg center-block">
+	        <input class="form-control" placeholder="Search" type="text">
+	        <span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
+	      </div>
+	    </div>
+  	</form>
+
+	<a href="#myCarousel" class="left carousel-control" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+	<a href="#myCarousel" class="right carousel-control" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div><!-- myCarousel -->
